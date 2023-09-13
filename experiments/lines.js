93ce4c065a28068aa52c6780d2ab7dc462e3297f
Kimpes <gustafssonfilip@hotmail.com>
2023-09-13 13:46:22 +0200

    Added lines file
    it doesn't contain anything yet, but might as well push it

---
+++ experiments/lines.js
@@ -0,0 +1,17 @@
+const SQUARE_COUNT = 14;
+const INTENSITY = 2;
+const WEIGHT = 10;
+const RATE = 1;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background(255, 255, 255);
+}
+
+function draw() {
+  
+}
+
+function mouseClicked() {
+
+}
