6465796dcda20b6ec51b2647e277a82b34196874
Kimpes <gustafssonfilip@hotmail.com>
2023-09-19 13:16:51 +0200

    Fixed the second version of the tree experiment

--- experiments/tree-windy.js
+++ experiments/tree-windy.js
@@ -1,14 +1,10 @@
-const SQUARE_COUNT = 14;
-const INTENSITY = 2;
-const WEIGHT = 10;
-const RATE = 1;
-
 let branchList = [];
 const fieldSize = 50;
 const maxCols = Math.ceil(innerWidth / fieldSize);
 const maxRows = Math.ceil(innerHeight / fieldSize);
 const divider = 4;
 let field;
+let mouse = createVector(mouseX, mouseY);
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
@@ -16,11 +12,12 @@ function setup() {
   field = generateField();
 }
 
+// Branch Class --------------------------
 class TreeBranch {
   constructor(x, y, canSplit, lifespan, maxForce, maxSpeed) {
     this.position = createVector(x, y);
     this.velocity = createVector(random(-1, 1), 0);
-    this.acceleration = 0;
+    this.acceleration = createVector(0, 0);
     this.lifespan = lifespan;
     this.canSplit = canSplit;
     this.maxForce = maxForce;
@@ -33,40 +30,49 @@ class TreeBranch {
     ellipse(this.position.x, this.position.y, 2);
     pop();
   }
+  follow(desiredDirection) {
+    desiredDirection = desiredDirection.copy();
+    desiredDirection.mult(this.maxSpeed);
+    let steer = p5.Vector.sub(desiredDirection, this.velocity);
+    steer.limit(this.maxForce);
+    this.applyForce(steer);
+  }
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+  checkBorders() {
+    if (this.position.x <= 0) {
+      this.position.x = innerWidth;
+    } else if (this.position.x >= innerWidth) {
+      this.position.x = 0;
+    }
+    if (this.position.y <= 0) {
+      this.position.y = innerHeight;
+    } else if (this.position.y >= innerHeight) {
+      this.position.y = 0;
+    }
+  }
   multiply() {
     let newBranch;
     if (random(0, 100) < 10 - branchList.length) {
-      newBranch = new TreeBranch(this.position.x, this.position.y, true, this.lifespan + random(-50, 50));
+      newBranch = new TreeBranch(this.position.x, this.position.y, true, this.lifespan + random(-50, 50), 0.1, 4);
     }else{
-      newBranch = new TreeBranch(this.position.x, this.position.y, false, this.lifespan + random(-50, 200));
+      newBranch = new TreeBranch(this.position.x, this.position.y, false, this.lifespan + random(-50, 200), 0.1, 4);
     }
     branchList.push(newBranch);
   }
   update() {
-    //cited from Garritt's lecture on vectors and complexity
-    if (this.position.x < 0 || this.position.x > innerWidth) {
-      this.velocity.x *= -1;
-    }
-
-    if (this.position.y < 0 || this.position.y > innerWidth) {
-      this.velocity.y *= -1;
-    }
-
     if (random(0, this.lifespan) > this.lifespan - 5 && this.canSplit) {
       this.multiply();
     }
-
-    this.acceleration = p5.Vector.sub(mouse, this.position);
-    this.acceleration.normalize();
-    this.acceleration.mult(0.05);
-    this.acceleration.y *= random(0, 1);
-    this.acceleration.x *= random(-1, 1);
+    
     if (this.acceleration.y > 0) {
       this.acceleration.y *= -1;
     }
     this.velocity.add(this.acceleration);
     this.velocity.limit(1);
     this.position.add(this.velocity);
+    this.acceleration.mult(0);
     this.lifespan--;
   }
   isDead() {
@@ -76,14 +82,23 @@ class TreeBranch {
   }
 }
 
-let mouse = createVector(mouseX, mouseY);
+
+// Draw Function ---------------------------
 function draw() {
   mouse = createVector(mouseX, mouseY);
 
-  for (let branch of branchList) {
+  for (let id in branchList) {
+    let branch = branchList[id];
     if (!branch.isDead()){
+      branch.checkBorders();
+      const x = Math.floor(branch.position.x / fieldSize);
+      const y = Math.floor(branch.position.y / fieldSize);
+      const desiredDirection = field[x][y];
+      branch.follow(desiredDirection);
       branch.draw();
       branch.update();
+    } else {
+      branchList.splice(id, 1);
     }
   }
 }
